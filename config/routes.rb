b74b15f9ab056af89d432662e92cc10c47b2edba
Hedy <hedywoo@gmail.com>
2016-06-08 14:29:35 -0600

    Victory twittter log

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "welcome#index"
   get "/auth/twitter", as: :twitter_login
+  get "/auth/twitter/callback", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
